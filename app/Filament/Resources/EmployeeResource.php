292845308bae024cb4b70e9c7ece395ad2211a03
RanuPutra <raya.ranuputra@gmail.com>
2025-05-21 14:28:18 +0700

    feat(EmployeeResource): update form and table with employee details

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -3,27 +3,40 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\EmployeeResource\Pages;
-use App\Filament\Resources\EmployeeResource\RelationManagers;
 use App\Models\Employee;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class EmployeeResource extends Resource
 {
     protected static ?string $model = Employee::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    
+    protected static ?string $navigationLabel = 'Karyawan';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('id_karyawan')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+                Forms\Components\TextInput::make('nama')
+                    ->required(),
+                Forms\Components\TextInput::make('jabatan'),
+                Forms\Components\TextInput::make('departemen'),
+                Forms\Components\TextInput::make('email')
+                    ->email(),
+                Forms\Components\TextInput::make('nomor_telepon'),
+                Forms\Components\Textarea::make('alamat'),
+                Forms\Components\DatePicker::make('tanggal_bergabung'),
+                Forms\Components\FileUpload::make('foto')
+                    ->image()
+                    ->directory('employees')
             ]);
     }
 
@@ -31,13 +44,26 @@ class EmployeeResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('id_karyawan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nama')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('jabatan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('departemen')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('nomor_telepon'),
+                Tables\Columns\ImageColumn::make('foto'),
+                Tables\Columns\TextColumn::make('tanggal_bergabung')
+                    ->date(),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
